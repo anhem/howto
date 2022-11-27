944fc2157f95beea66b0aa49ef1b8e84da6c397e
Andreas Hemmingsson <andhem@gmail.com>
2022-11-27 23:34:29 +0100

    refactoring

---
+++ src/main/java/com/github/anhem/howto/aggregator/AccountAggregator.java
@@ -0,0 +1,31 @@
+package com.github.anhem.howto.aggregator;
+
+import com.github.anhem.howto.controller.model.AccountDetailsDTO;
+import com.github.anhem.howto.model.Account;
+import com.github.anhem.howto.model.id.AccountId;
+import com.github.anhem.howto.model.id.RoleName;
+import com.github.anhem.howto.repository.AccountRoleRepository;
+import com.github.anhem.howto.service.AccountService;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static com.github.anhem.howto.controller.mapper.AccountDetailsDTOMapper.mapToAccountDetailsDTO;
+
+@Component
+public class AccountAggregator {
+
+    private final AccountService accountService;
+    private final AccountRoleRepository accountRoleRepository;
+
+    public AccountAggregator(AccountService accountService, AccountRoleRepository accountRoleRepository) {
+        this.accountService = accountService;
+        this.accountRoleRepository = accountRoleRepository;
+    }
+
+    public AccountDetailsDTO getAccountDetails(AccountId accountId) {
+        Account account = accountService.getAccount(accountId);
+        List<RoleName> roleNames = accountRoleRepository.getRoleNames(accountId);
+        return mapToAccountDetailsDTO(account, roleNames);
+    }
+}
--- src/main/java/com/github/anhem/howto/controller/AccountController.java
+++ src/main/java/com/github/anhem/howto/controller/AccountController.java
@@ -1,14 +1,12 @@
 package com.github.anhem.howto.controller;
 
+import com.github.anhem.howto.aggregator.AccountAggregator;
 import com.github.anhem.howto.controller.model.AccountDetailsDTO;
 import com.github.anhem.howto.controller.model.AccountsDTO;
 import com.github.anhem.howto.controller.model.CreateAccountDTO;
 import com.github.anhem.howto.controller.model.MessageDTO;
-import com.github.anhem.howto.model.Account;
 import com.github.anhem.howto.model.id.AccountId;
 import com.github.anhem.howto.model.id.Password;
-import com.github.anhem.howto.model.id.RoleName;
-import com.github.anhem.howto.repository.AccountRoleRepository;
 import com.github.anhem.howto.service.AccountService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
-import static com.github.anhem.howto.controller.mapper.AccountDetailsDTOMapper.mapToAccountDetailsDTO;
 import static com.github.anhem.howto.controller.mapper.AccountsDTOMapper.mapToAccountsDTO;
 import static com.github.anhem.howto.controller.mapper.CreateAccountDTOMapper.mapToAccount;
 
@@ -28,14 +24,14 @@ import static com.github.anhem.howto.controller.mapper.CreateAccountDTOMapper.ma
 public class AccountController {
 
     private final AccountService accountService;
-    private final AccountRoleRepository accountRoleRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AccountAggregator accountAggregator;
 
     @Autowired
-    public AccountController(AccountService accountService, AccountRoleRepository accountRoleRepository, PasswordEncoder passwordEncoder) {
+    public AccountController(AccountService accountService, PasswordEncoder passwordEncoder, AccountAggregator accountAggregator) {
         this.accountService = accountService;
-        this.accountRoleRepository = accountRoleRepository;
         this.passwordEncoder = passwordEncoder;
+        this.accountAggregator = accountAggregator;
     }
 
     @GetMapping
@@ -45,10 +41,7 @@ public class AccountController {
 
     @GetMapping(value = "/details/{accountId}")
     public AccountDetailsDTO getAccountDetails(@PathVariable int accountId) {
-        AccountId accId = new AccountId(accountId);
-        Account account = accountService.getAccount(accId);
-        List<RoleName> roleNames = accountRoleRepository.getRoleNames(accId);
-        return mapToAccountDetailsDTO(account, roleNames);
+        return accountAggregator.getAccountDetails(new AccountId(accountId));
     }
 
     @DeleteMapping(value = "{accountId}")
@@ -59,14 +52,12 @@ public class AccountController {
 
     @PostMapping(value = "users/user")
     public MessageDTO createUserAccount(@Valid @RequestBody CreateAccountDTO createAccountDTO) {
-        return MessageDTO.fromId(accountService.createUserAccount(mapToAccount(createAccountDTO),
-                new Password(passwordEncoder.encode(createAccountDTO.getPassword()))));
+        return MessageDTO.fromId(accountService.createUserAccount(mapToAccount(createAccountDTO), new Password(passwordEncoder.encode(createAccountDTO.getPassword()))));
     }
 
     @PostMapping(value = "users/administrator")
     public MessageDTO createAdministratorAccount(@Valid @RequestBody CreateAccountDTO createAccountDTO) {
-        return MessageDTO.fromId(accountService.createAdministratorAccount(mapToAccount(createAccountDTO),
-                new Password(passwordEncoder.encode(createAccountDTO.getPassword()))));
+        return MessageDTO.fromId(accountService.createAdministratorAccount(mapToAccount(createAccountDTO), new Password(passwordEncoder.encode(createAccountDTO.getPassword()))));
     }
 
 }
